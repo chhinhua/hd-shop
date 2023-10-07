69a3e19ee31582ecf83846a5aae92fbde602bc5a
Chhin Hua <chhinhua@gmail.com>
2023-10-07 15:43:01 +0700

    create repository classes

---
+++ src/main/java/com/hdshop/repositories/AddressRepository.java
@@ -0,0 +1,9 @@
+package com.hdshop.repositories;
+
+import com.hdshop.entities.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Long> {
+}
